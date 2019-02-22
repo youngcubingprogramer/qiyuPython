8df28b988858dd7cf9a6a14a2f01ab4097e5e273
kirahan <hanzhaoapply@gmail.com>
2019-02-22 12:24:48 +0800

    add class2 homework

---
+++ class2/td1.py
@@ -0,0 +1,14 @@
+# 1. Two Sum
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# Example:
+#
+# Given nums = [2, 7, 11, 15], target = 9,
+#
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+nums = [73, 23, 13, 27, 40, 54, 27, 7, 47, 88, 85, 63, 20, 57, 26, 17, 51, 91, 28, 51, 27, 82, 31, 21, 99, 69, 58, 78, 85, 1, 64, 92, 37, 53, 7, 48, 87, 94, 88, 34, 77, 80, 2, 53, 70, 39, 97, 20, 54, 1, 14, 44, 69, 17, 81, 13, 4, 16, 40, 76, 75, 0, 92, 19, 89, 55, 52, 23, 58, 38, 17, 31, 62, 76, 5, 27, 23, 43, 67, 42, 60, 72, 76, 2, 63, 77, 40, 53, 92, 52, 67, 52, 21, 85, 1, 58, 47, 0, 66, 92]
+target = 50
